d90b0380c882409b98a2524ce8660ae04fed922b
Konstantin kokadevops <kokadevops@gmail.com>
2023-06-10 23:31:12 +0300

    add Categories and SingleProduct

---
+++ src/components/Products/SingleProduct.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+
+import { useNavigate, useParams } from "react-router-dom";
+
+import { useGetProductQuery } from "../../features/api/apiSlice";
+
+import { ROUTES } from "../../utils/routes";
+
+const SingleProduct = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const { data, isLoading, isFetching, isSuccess } = useGetProductQuery({
+        id,
+    });
+    useEffect(() => {
+        if (!isFetching && !isLoading && !isSuccess) {
+            navigate(ROUTES.HOME);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isLoading, isFetching, isSuccess]);
+
+    return <div>SingleProduct</div>;
+};
+
+export default SingleProduct;
--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
+import SingleProduct from "../Products/SingleProduct";
+import { ROUTES } from "../../utils/routes";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route index element={<Home />} />
+            <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
         </Routes>
     );
 };
